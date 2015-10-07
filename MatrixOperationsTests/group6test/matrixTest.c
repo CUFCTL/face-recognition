b27d0b065a31f60fe65ace3d591c8939c53a830e
Colin Targonski <ctargon@g.clemson.edu>
2015-10-06 22:37:08 -0400

    Updated matrixTest.c for group 6 testing.
    The matrixTest.c file will not compile because of implicit
    declarations throughout the function calls from the gsl library.
    These functions need to be rewritten with the new library or the
    gsl library needs to be included so the functions can be declared
    and the test cases can compile.
    
    
    Former-commit-id: aad775a0cd2f5682dc06f274878bca4f2fb4d25e

--- MatrixOperationsTests/group6test/matrixTest.c
+++ MatrixOperationsTests/group6test/matrixTest.c
@@ -11,15 +11,28 @@ int main(void)
     FILE *output = fopen ("testResults.txt", "w");
 
     // declare sizes for different test matrices
-    int test1 = 2, test2 = 3, test3 = 5;
+    int test1 = 5;
     matrix_t *M = m_initialize (FILL, test1, test1);
-    matrix_t *eigenValues = m_initialize(FILL, test1, test1);
-    matrix_t *eigenVectors = m_initialize(FILL, test1, test1);
+    matrix_t *sub = NULL;
+    //matrix_t *eigenValues = m_initialize(FILL, test1, test1);
+    //matrix_t *eigenVectors = m_initialize(FILL, test1, test1);
 
     // print starting matrix
 	fprintf (output, "M = \n");
 	m_fprint (output, M);
 
+    // make a sub matrix
+    sub = m_getSubMatrix(M, 0, 0, 2, 2);
+    fprintf(output, "\nsub from [0,0] to [2,2] = \n");
+    m_fprint(output, sub);
+
+
+
+
+/***************************************************************************
+    EIGENVALUE FUNCTION NOT WORKING BECAUSE OF GSL LIBRARY NOT BEING INCLUDED
+    SO COMMENTING OUT THIS TEST
+****************************************************************************
     // Test for eigenvalues/eigenvectors
     fprintf (output, "\n-------------Test Group 1: Size = 2 -------------\n");
     m_eigenvalues_eigenvectors (M, &eigenValues, &eigenVectors);
@@ -55,6 +68,7 @@ int main(void)
     m_free(M);
     m_free(eigenValues);
     m_free(eigenVectors);
+    */
 
     return 0;
 
