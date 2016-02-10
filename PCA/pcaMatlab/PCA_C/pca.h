ad0fe86387ef112fb05d8b37290f5c12a80f207d
gfitzma <gfitzma@g.clemson.edu>
2016-02-10 14:45:47 -0500

    Added matlab code for PCA
    Former-commit-id: ce7bb0ea1d3be1b8f5a12f99ca1b975e3f8844dc

---
+++ PCA/pcaMatlab/PCA_C/pca.h
@@ -0,0 +1,28 @@
+/*
+ *	pca.h
+ *
+ *	Description: 
+ *
+ */
+
+#ifndef pca_header
+#define pca_header
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define eigen_type float /* types of values for eigenfaces and projected images matrices */
+
+#define DEBUG 0 /* 1 for printf debug statements */
+#define PROFILING 0 /* 1 if testing recognition time */
+
+extern void LoadTrainingDatabase(char *filename, eigen_type **projectedimages,
+        eigen_type **eigenfacesT, eigen_type **mean, long int *images, long int *imgsize,
+        long int *num_faces);
+
+extern void Recognition(char *inputimage, eigen_type **mean, eigen_type **projectedimages,
+        eigen_type **eigenfacesT, eigen_type **projectedtrainimages, long int *images,
+        long int *imgsize, long int *num_faces);
+
+#endif
+
