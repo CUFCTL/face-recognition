816450af840e44aca725da8efb4983ef545fed1e
Ben Shealy <bentshermann@gmail.com>
2016-06-29 16:58:57 -0400

    Implemented sphere function in shared code
    Former-commit-id: f3dcc46fbb8c14216dc83886b79fa1f0721aad20

--- src/ica.c
+++ src/ica.c
@@ -7,6 +7,32 @@
 #include "matrix.h"
 
 /**
+ * Apply a "sphere" transformation to a vector.
+ *
+ * @param x  pointer to column vector
+ * @return pointer to the transformed vector
+ */
+matrix_t * sphere(matrix_t *x)
+{
+    // TODO: subtract mean from x
+
+    // compute W_z = 2 * Cov(X)^(-1/2)
+    matrix_t *W_z_temp1 = m_covariance(x);
+    matrix_t *W_z_temp2 = m_sqrtm(W_z_temp1);
+    matrix_t *W_z = m_inverse(W_z_temp2);
+    m_elem_mult(W_z, 2);
+
+    // compute x_out = W_z * x
+    matrix_t *x_out = m_product(W_z, x);
+
+    free(W_z_temp1);
+    free(W_z_temp2);
+    free(W_z);
+
+    return x_out;
+}
+
+/**
  * Compute the projection matrix of a training set with ICA.
  *
  * @param A  mean-subtracted image matrix
