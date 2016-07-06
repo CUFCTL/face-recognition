74e6cea38fbc7289f45b43f5433f241cabf68114
Ben Shealy <bentshermann@gmail.com>
2016-07-06 11:33:26 -0400

    Reorganized archive, removed old unused code

--- archive/PCA/EIGProjectionMethod/ImpulseC Version/_Make_ImpulsePCA.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-set PATH=%IMPULSEC_HOME%\bin;%IMPULSEC_GCC_HOME%\bin;%PATH%;
-set IMPULSEC_HOME=C:/Impulse/CoDeveloper3
-set IMPULSEC_GCC_HOME=%IMPULSEC_HOME%/MinGW
-"C:\Impulse\CoDeveloper3\bin\make.exe" SHELL=C:/Impulse/CoDeveloper3/bin/sh.exe -f"C:\Documents and Settings\smithmc\Desktop\mneeley\old_impulse_c\_Makefile" build
-echo 1 > "CoDeveloper.log.done"
