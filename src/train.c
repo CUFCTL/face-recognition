a18f075e4a07affc8c99f21696eec3609891a9be
Colin <ctargon@g.clemson.edu>
2016-07-06 22:59:37 -0400

    adding getopt_long_only to determine which algorithms to use

--- src/train.c
+++ src/train.c
@@ -4,14 +4,28 @@
  * Train a face database with a set of images.
  */
 #include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
 #include "database.h"
 
 int main(int argc, char **argv)
 {
-	if ( argc != 2 ) {
-		fprintf(stderr, "usage: ./train [images-folder]\n");
-		return 1;
+	static struct option long_options[] = {
+		{"pca", no_argument, 0,  'p' },
+	  {"lda", no_argument, 0,  'l' },
+	  {"ica", no_argument, 0,  'i' },
+		{"all", no_argument, 0,  'a' },
+	  {0,     0,           0,   0  }
+	};
+
+	while(opt = getopt_long_only(argc, argv, "", long_options, &options_index) != -1)
+	{
+		switch (opt)
+		{
+
+		}
 	}
+
 
 	const char *TRAINING_SET_PATH = argv[1];
 	const char *DB_TRAINING_SET = "./db_training_set.dat";
