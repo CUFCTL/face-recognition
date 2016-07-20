d72fa5778d3ba5b60ff8beced98221eefaaaf137
Ben Shealy <bentshermann@gmail.com>
2016-07-20 14:36:33 -0400

    Added statistics for recognition results

--- MATLAB/LDA/example.m
+++ MATLAB/LDA/example.m
@@ -25,6 +25,8 @@ T = CreateDatabase(TrainDatabasePath);
 TrainFiles = dir(strcat(TrainDatabasePath, '/*.ppm'));
 TestFiles = dir(strcat(TestDatabasePath, '/*.ppm'));
 
+num_correct = 0;
+
 for i = 1 : size(TestFiles, 1)
     % perform recognition algorithm
     strtest = strcat(TestDatabasePath, '/', TestFiles(i).name);
@@ -34,4 +36,17 @@ for i = 1 : size(TestFiles, 1)
     fprintf('test image: \"%s\"\n', TestFiles(i).name);
     fprintf('       LDA: \"%s\"\n', TrainFiles(j).name);
     fprintf('\n');
+
+    % determine whether the algorithm was correct
+    % assumes that filename is formatted as '{class}_{index}.ppm'
+    tokens_train = strsplit(TrainFiles(j).name, '_');
+    tokens_test = strsplit(TestFiles(i).name, '_');
+
+    if strcmp(tokens_train{1}, tokens_test{1})
+        num_correct = num_correct + 1;
+    end
 end
+
+success_rate = num_correct / size(TestFiles, 1) * 100;
+
+fprintf('%d / %d matched, %.2f%%\n', num_correct, size(TestFiles, 1), success_rate);
--- MATLAB/PCA/example.m
+++ MATLAB/PCA/example.m
@@ -28,6 +28,8 @@ T = CreateDatabase(TrainDatabasePath);
 ProjectedImages = Eigenfaces' * A;
 
 % test each image in the test set
+num_correct = 0;
+
 for i = 1 : size(TestFiles, 1)
     % perform recognition algorithm
     strtest = strcat(TestDatabasePath, '/', TestFiles(i).name);
@@ -37,4 +39,17 @@ for i = 1 : size(TestFiles, 1)
     fprintf('test image: \"%s\"\n', TestFiles(i).name);
     fprintf('       PCA: \"%s\"\n', TrainFiles(j).name);
     fprintf('\n');
+
+    % determine whether the algorithm was correct
+    % assumes that filename is formatted as '{class}_{index}.ppm'
+    tokens_train = strsplit(TrainFiles(j).name, '_');
+    tokens_test = strsplit(TestFiles(i).name, '_');
+
+    if strcmp(tokens_train{1}, tokens_test{1})
+        num_correct = num_correct + 1;
+    end
 end
+
+success_rate = num_correct / size(TestFiles, 1) * 100;
+
+fprintf('%d / %d matched, %.2f%%\n', num_correct, size(TestFiles, 1), success_rate);
