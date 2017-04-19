21f01ab312e9857e7aec7bf2f0c1b429b2b77281
Ben Shealy <bentshermann@gmail.com>
2017-04-18 23:52:46 -0400

    Added timestamp to logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -5,6 +5,7 @@
  */
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
 #include "logger.h"
 
 logger_level_t LOGLEVEL = LL_INFO;
@@ -25,6 +26,13 @@ void log(logger_level_t level, const char *format, ...)
 			? stderr
 			: stdout;
 		va_list ap;
+
+		time_t t = time(NULL);
+		struct tm *tm = localtime(&t);
+
+		fprintf(stream, "%04d-%02d-%02d %02d:%02d:%02d ",
+			1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday,
+			tm->tm_hour, tm->tm_min, tm->tm_sec);
 
 		va_start(ap, format);
 		vfprintf(stream, format, ap);
