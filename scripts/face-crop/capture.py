b6a7958e5b18f43c9319e6d35e820a19aa50a29d
jtetrea <jtetrea@clemson.edu>
2017-09-25 17:31:23 -0400

    Changed facecrop scripts to use tabs, resize each image to 128x128 before saving

--- scripts/face-crop/capture.py
+++ scripts/face-crop/capture.py
@@ -6,7 +6,7 @@ import crop
 # define constants
 FOLDER_OUT = "FOLDER_OUT"
 DEVICE_NUM = 0
-NUM_FRAMES = 1
+NUM_FRAMES = 5
 
 # load the cascade classifier files
 cascade_face = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
@@ -14,7 +14,7 @@ cascade_face = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 # initialize output directory
 if not os.path.exists(FOLDER_OUT):
-    os.mkdir(FOLDER_OUT, 0775)
+	os.mkdir(FOLDER_OUT, 0775)
 
 # initialize video feed
 cap = cv2.VideoCapture(DEVICE_NUM)
@@ -22,24 +22,24 @@ cap = cv2.VideoCapture(DEVICE_NUM)
 # begin capture/crop loop
 i = 0
 while True:
-    # capture frame
-    ret, frame = cap.read()
+	# capture frame
+	ret, frame = cap.read()
 
-    # detect and draw bounding box for each face
-    faces = crop.detect_face(frame, cascade_face)
-    img = crop.box_face(frame, faces)
+	# detect and draw bounding box for each face
+	faces = crop.detect_face(frame, cascade_face)
+	img = crop.box_face(frame, faces)
 
-    # save cropped image every NUM_FRAMES
-    i = i + 1
-    if i == NUM_FRAMES:
-        crop.crop_face(frame, faces, FOLDER_OUT)
-        i = 0
+	# save cropped image every NUM_FRAMES
+	i = i + 1
+	if i == NUM_FRAMES:
+		crop.crop_face(frame, faces, FOLDER_OUT)
+		i = 0
 
-    # display the annotated frame
-    cv2.imshow("Face Detection", img)
+	# display the annotated frame
+	cv2.imshow("Face Detection", img)
 
-    if (cv2.waitKey(1) & 0xFF) == ord("q"):
-        break
+	if (cv2.waitKey(1) & 0xFF) == ord("q"):
+		break
 
 # release the video feed
 cap.release()
--- scripts/face-crop/crop.py
+++ scripts/face-crop/crop.py
@@ -3,9 +3,7 @@
 import cv2
 from datetime import datetime
 
-
 def detect_face(img, cascade_face):
-
 	# read the input image and convert to grayscale
 	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -28,13 +26,14 @@ def box_face(img, faces):
 def crop_face(img, faces, FOLDER_OUT):
 	# save each face to a cropped image, and draw rectangles
 	for (x,y,w,h) in faces:
-	    path = FOLDER_OUT + "/" + str(datetime.now()) + ".jpg"
-	    print path
+		path = FOLDER_OUT + "/" + str(datetime.now()) + ".jpg"
+		print path
 
-	    x1 = x
-	    x2 = x + w
-	    y1 = y
-	    y2 = y + h
+		x1 = x
+		x2 = x + w
+		y1 = y
+		y2 = y + h
 
-	    roi = img[y1:y2, x1:x2]
-	    cv2.imwrite(path, roi)
+		roi = img[y1:y2, x1:x2]
+		final_img = cv2.resize(roi, (128,128))
+		cv2.imwrite(path, final_img)
