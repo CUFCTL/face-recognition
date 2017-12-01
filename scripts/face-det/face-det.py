8c1b6f02832d5c70631af18264972e5f6ca1803c
Ben Shealy <bentshermann@gmail.com>
2017-12-01 13:46:55 -0500

    Refactored face-det.py to just do detection and cropping

--- scripts/face-det/face-det.py
+++ scripts/face-det/face-det.py
@@ -1,19 +1,47 @@
 import cv2
+import datetime
 import os
 import shutil
 import signal
 import sys
 import threading
-import classifier
-import detector
 
 # define constants
 DEVICE_NUM = 0
-
-TRAIN_DATA = "../../train_data"
-STREAM_DATA = "test_data"
-
+OUTPUT_DIR = "test_data"
 MAX_FPS = 30
+
+IMAGE_SIZE = (128, 128)
+RECT_COLOR = (255, 0, 0)
+RECT_LINEWIDTH = 2
+
+def detect_faces(image, cascade_face):
+	# read the input image and convert to grayscale
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# run the detection function on the grayscale image
+	faces = cascade_face.detectMultiScale(image, 1.3, 5)
+
+	return faces
+
+def label_faces(image, faces):
+	# draw bounding box for each face
+	for (x,y,w,h) in faces:
+		cv2.rectangle(image, (x,y), (x+w,y+h), RECT_COLOR, RECT_LINEWIDTH)
+
+	return image
+
+def crop_faces(image, faces, path):
+	# crop and save each face to a file
+	for (x,y,w,h) in faces:
+		filename = "%s/%s.ppm" % (path, str(datetime.datetime.now()))
+		filename = filename.replace(" ", "_")
+
+		print "Saving '%s'..." % filename
+
+		image_out = image[y:(y+h), x:(x+w)]
+		image_out = cv2.resize(image_out, IMAGE_SIZE)
+		cv2.imwrite(filename, image_out)
 
 def display():
 	# set the next timer call
@@ -34,24 +62,14 @@ def display():
 		quit()
 
 	# detect faces
-	faces = detector.detect_faces(frame, cascade_face)
+	faces = detect_faces(frame, cascade_face)
 
 	if len(faces) > 0:
-		# prepare stream directory
-		if os.path.exists(STREAM_DATA):
-			shutil.rmtree(STREAM_DATA)
-
-		if not os.path.exists(STREAM_DATA):
-			os.mkdir(STREAM_DATA, 0755)
-
-		# perform face recognition
-		detector.crop_faces(frame, faces, STREAM_DATA)
-		labels = server.predict(len(faces))
-
-		print "Hello, %s" % (" and ".join(labels))
+		# save each face to a file
+		crop_faces(frame, faces, OUTPUT_DIR)
 
 		# draw bounding boxes around each face
-		frame = detector.label_faces(frame, faces, labels)
+		frame = label_faces(frame, faces)
 
 	# display the annotated frame
 	cv2.imshow("Face Detection", frame)
@@ -67,22 +85,16 @@ def quit(signal=None, frame=None):
 		display.thread.cancel()
 	cap.release()
 	cv2.destroyAllWindows()
-	server.stop()
 	sys.exit()
 	print "\nBye"
 
 # load the cascade classifier files
-print "Loading cascade classifier files..."
-
 cascade_face = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 #cascade_eye = cv2.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
 
-# initialize face-rec server
-print "Initializing recognition server..."
-
-server = classifier.Server()
-server.train(TRAIN_DATA)
-server.start(STREAM_DATA)
+# prepare output directory
+if not os.path.exists(OUTPUT_DIR):
+	os.mkdir(OUTPUT_DIR, 0755)
 
 # initialize video stream
 cap = cv2.VideoCapture(DEVICE_NUM)
